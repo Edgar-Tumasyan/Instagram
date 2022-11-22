06f7b2ed34f1bf01a9c278b42a35a813049510f1
Edgar-Tumasyan <tumasyan.edgar@mail.ru>
2022-11-22 17:21:50 +0400

    commit

--- components/Cloudinary.js
+++ components/Cloudinary.js
@@ -1,4 +1,7 @@
+const config = require('../config');
 const cloudinary = require('cloudinary').v2;
+
+cloudinary.config(config.cloudinary);
 
 class Cloudinary {
   static async upload(file, path) {
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const app = new (require('koa'))();
-const cloudinary = require('cloudinary').v2;
 
 const config = require('./config');
 const Routes = require('./routes');
 
-cloudinary.config(config.cloudinary);
-
 const port = config.PORT || 3000;
 
-//////const fileType = require('file-type');
+const fileType = require('file-type');
 app.use(require('koa-bodyparser')());
 
 app.use(require('./middleware/requestNormalizer')());
