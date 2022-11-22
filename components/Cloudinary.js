8faae48691e2d8cd7715adaee1379f4ebd527f6e
Edgar-Tumasyan <tumasyan.edgar@mail.ru>
2022-11-22 11:28:35 +0400

    commit

--- components/Cloudinary.js
+++ components/Cloudinary.js
@@ -2,16 +2,14 @@ const cloudinary = require('cloudinary').v2;
 
 class Cloudinary {
   static async upload(file, path) {
-    const attachment = await cloudinary.uploader.upload(file, {
+    return await cloudinary.uploader.upload(file, {
       use_filename: true,
       folder: path,
     });
-
-    return attachment;
   }
 
   static async delete(file) {
-    await cloudinary.uploader.destroy(file);
+    return await cloudinary.uploader.destroy(file);
   }
 }
 
