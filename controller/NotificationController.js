f564aab7a8c47732018e73c9c31a103fd123b0ae
Edgar-Tumasyan <tumasyan.edgar@mail.ru>
2022-12-05 16:56:14 +0400

    commit

--- controller/NotificationController.js
+++ controller/NotificationController.js
@@ -1,4 +1,5 @@
 const { Notification, User } = require('../data/models');
+const { Op } = require('sequelize');
 
 const findAll = async ctx => {
     const receiverId = ctx.state.user.id;
@@ -8,8 +9,12 @@ const findAll = async ctx => {
 
     await Notification.update({ isSeen: true }, { where: { receiverId } });
 
+    const senderIds = notifications.map(notification => notification.senderId);
+
+    const users = await User.findAll({ where: { id: { [Op.in]: senderIds } }, raw: true });
+
     for (const notification of notifications) {
-        const user = await User.findByPk(notification.senderId, { raw: true });
+        const user = users.find(user => user.id === notification.senderId);
 
         notification.firstname = user.firstname;
         notification.lastname = user.lastname;
